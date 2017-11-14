7611cfbbedb5e49145783a9275f2e41cac04b3d5
Parthiv Krishna <parthiv.krishna@gmail.com>
2017-11-13 19:33:58 -0600

    Add motor ports to RobotMap

--- src/org/usfirst/frc/team3082/robot/RobotMap.java
+++ src/org/usfirst/frc/team3082/robot/RobotMap.java
@@ -6,14 +6,7 @@ package org.usfirst.frc.team3082.robot;
  * the wiring easier and significantly reduces the number of magic numbers
  * floating around.
  */
-public class RobotMap {
-	// For example to map the left and right motors, you could define the
-	// following variables to use with your drivetrain subsystem.
-	// public static int leftMotor = 1;
-	// public static int rightMotor = 2;
-
-	// If you are using multiple modules, make sure to define both the port
-	// number and the module. For example you with a rangefinder:
-	// public static int rangefinderPort = 1;
-	// public static int rangefinderModule = 1;
+public class RobotMap {	
+	public static int LEFT_MOTOR_PORT = 1;
+	public static int RIGHT_MOTOR_PORT = 0;
 }
