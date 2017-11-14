16008b74f8de06d0c3013f54d82e468d2295ea21
Parthiv Krishna <parthiv.krishna@gmail.com>
2017-11-13 18:28:46 -0600

    Create Joysticks in OI
    Driver and operator joysticks

--- src/org/usfirst/frc/team3082/robot/OI.java
+++ src/org/usfirst/frc/team3082/robot/OI.java
@@ -1,18 +1,16 @@
 package org.usfirst.frc.team3082.robot;
 
-import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.Joystick;
+
 
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-	//// CREATING BUTTONS
-	// One type of button is a joystick button which is any button on a
-	//// joystick.
-	// You create one by telling it which joystick it's on and which button
-	// number it is.
-	// Joystick stick = new Joystick(port);
+
+	Joystick driver = new Joystick(0);
+	Joystick operator = new Joystick(0);
 	// Button button = new JoystickButton(stick, buttonNumber);
 
 	// There are a few additional built in buttons you can use. Additionally,
